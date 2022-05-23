96e9158171dc640bdab4d0196ad3c5266015533a
Andrew Markham-Davies <atmd1983@gmail.com>
2022-05-23 09:22:38 +0100

    update

--- promo-page/src/App.tsx
+++ promo-page/src/App.tsx
@@ -7,7 +7,7 @@ import Avatar from "@mui/material/Avatar";
 import Container from "@mui/material/Container";
 import { deepOrange } from "@mui/material/colors";
 import Typography from "@mui/material/Typography";
-
+import TextField from "@mui/material/TextField";
 import { Parallax } from "react-parallax";
 
 import AppBar from "@mui/material/AppBar";
@@ -85,12 +85,12 @@ const people = [
     role: "Digital Transformation Director",
     linkedin: "https://www.linkedin.com/in/clarewatson12/",
   },
-  {
-    name: "ANDREW MARKHAM-DAVIES",
-    image: "",
-    role: "Blockchain & Tokenisation Specialist",
-    linkedin: null,
-  },
+  // {
+  //   name: "ANDREW MARKHAM-DAVIES",
+  //   image: "",
+  //   role: "Blockchain & Tokenisation Specialist",
+  //   linkedin: null,
+  // },
   {
     name: "RICH COLVILL",
     image: "/profiles/Rich-Colvill.jpg",
@@ -142,7 +142,7 @@ const people = [
   },
 ];
 
-const pages = ["Vision", "Technology", "Team"];
+const pages = ["Vision", "Contact", "Team", "Technology"];
 
 function App() {
   return (
@@ -185,7 +185,7 @@ function App() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            <img height="40" src="/Stelthio_lock-up-light.png" alt="" />
           </Typography>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
@@ -293,9 +293,9 @@ function App() {
         sx={{ padding: "5rem 0 5rem", background: "#18191d" }}
       >
         <Grid container spacing={2}>
-          <Grid item xs={6} md={4} sx={{ padding: "0 3rem" }}>
+          <Grid item xs={12} md={4} sx={{ padding: { xs: "0", md: "0 3rem" } }}>
             <Typography
-              sx={{ textAlign: "center", color: "#e8e8e8" }}
+              sx={{ textAlign: "center", color: "#e8e8e8", padding: "0 1rem" }}
               component="p"
             >
               STEALTHIO is designed by talent experts, for recruitment industry
@@ -303,9 +303,9 @@ function App() {
               in ownership of the company.
             </Typography>
           </Grid>
-          <Grid item xs={6} md={4} sx={{ padding: "0 3rem" }}>
+          <Grid item xs={12} md={4} sx={{ padding: { xs: "0", md: "0 3rem" } }}>
             <Typography
-              sx={{ textAlign: "center", color: "#e8e8e8" }}
+              sx={{ textAlign: "center", color: "#e8e8e8", padding: "0 1rem" }}
               component="p"
             >
               STEALTHIO is far more than just another combined CRM with ATS,
@@ -313,9 +313,9 @@ function App() {
               isn’t another expensive API plug-in tool to add to
             </Typography>
           </Grid>
-          <Grid item xs={12} md={4} sx={{ padding: "0 3rem" }}>
+          <Grid item xs={12} md={4} sx={{ padding: { xs: "0", md: "0 3rem" } }}>
             <Typography
-              sx={{ textAlign: "center", color: "#e8e8e8" }}
+              sx={{ textAlign: "center", color: "#e8e8e8", padding: "0 1rem" }}
               component="p"
             >
               STEALTHIO is here to completely transform and revolutionise the
@@ -328,22 +328,43 @@ function App() {
 
       <Container
         id="vision"
-        sx={{ padding: "5rem 0 5rem", background: "#18191d" }}
+        maxWidth={false}
+        sx={{
+          padding: "0",
+          background: "#1e132c",
+        }}
       >
         <Grid container spacing={2}>
-          <Grid item xs={12}>
+          <Grid item md={6} sx={{ display: { xs: "none", md: "block" } }}>
+            <img
+              style={{ width: "100%", height: "100%" }}
+              src="/Stelthio_graphic-1.png"
+              alt=""
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
             <Box
               sx={{
-                textAlign: "center",
-                alignItems: "center",
+                textAlign: "left",
+                alignItems: "left",
                 display: "flex",
                 flexDirection: "column",
+                paddingLeft: "3rem",
               }}
             >
               <Typography
                 variant="h5"
-                component="h4"
-                sx={{ marginBottom: "2rem", color: "#e8e8e8" }}
+                component="p"
+                sx={{
+                  marginBottom: "2rem",
+                  color: "#e8e8e8",
+                  textAlign: "left",
+                }}
               >
                 VISON
               </Typography>
@@ -351,7 +372,7 @@ function App() {
               <Typography
                 component="p"
                 sx={{
-                  padding: "0 6rem",
+                  padding: "0 6rem 0 0",
                   marginBottom: "1rem",
                   color: "#e8e8e8",
                 }}
@@ -360,25 +381,10 @@ function App() {
                 web 3.0 native, end-to-end, all-in- one, fully immersive talent
                 management ecosystem.
               </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
 
-      <Container sx={{ padding: "5rem 0 5rem", background: "#18191d" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Box
-              sx={{
-                textAlign: "center",
-                alignItems: "center",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
               <Typography
                 variant="h5"
-                component="h4"
+                component="p"
                 sx={{ marginBottom: "2rem", color: "#e8e8e8" }}
               >
                 MISSION
@@ -387,7 +393,7 @@ function App() {
               <Typography
                 component="p"
                 sx={{
-                  padding: "0 6rem",
+                  padding: "0 6rem 0 0",
                   marginBottom: "1rem",
                   color: "#e8e8e8",
                 }}
@@ -398,7 +404,11 @@ function App() {
 
               <Typography
                 component="p"
-                sx={{ padding: "0 6rem", marginBottom: "1rem" }}
+                sx={{
+                  padding: "0 6rem 0 0",
+                  marginBottom: "1rem",
+                  color: "#e8e8e8",
+                }}
               >
                 To enable recruiters to step-change operational efficiencies and
                 revenues
@@ -406,7 +416,11 @@ function App() {
 
               <Typography
                 component="p"
-                sx={{ padding: "0 6rem", marginBottom: "1rem" }}
+                sx={{
+                  padding: "0 6rem 0 0",
+                  marginBottom: "1rem",
+                  color: "#e8e8e8",
+                }}
               >
                 To inspire and enable, engaging and fully immersive candidate
                 experiences
@@ -416,98 +430,175 @@ function App() {
         </Grid>
       </Container>
 
-      <Container id="technology" sx={{ padding: "5rem 0 5rem" }}>
+      <Container
+        id="contact"
+        sx={{
+          padding: "5rem 0 5rem",
+          background: "#e8e8e8",
+        }}
+        maxWidth={false}
+      >
         <Grid container spacing={2}>
-          <Grid item xs={12}>
+          <Grid item xs={12} md={6}>
             <Box
               sx={{
-                textAlign: "center",
-                alignItems: "center",
+                textAlign: "left",
+                alignItems: "left",
                 display: "flex",
                 flexDirection: "column",
+                padding: { xs: "3rem", md: "0" },
               }}
             >
               <Typography
+                component="p"
                 variant="h5"
-                component="h4"
-                sx={{ marginBottom: "2rem" }}
+                sx={{
+                  padding: "0",
+                  marginBottom: "1rem",
+                  color: "#18191d",
+                }}
               >
-                TECHNOLOGY
+                Recruiter or Recruitment Business Owner?
+              </Typography>
+              <Typography
+                component="p"
+                variant="h6"
+                sx={{
+                  padding: "0",
+                  marginBottom: "1rem",
+                  color: "#18191d",
+                }}
+              >
+                Get in on the action!
               </Typography>
 
               <Typography
                 component="p"
-                sx={{ padding: "0 6rem", marginBottom: "1rem" }}
+                sx={{
+                  padding: "0",
+                  marginBottom: "1rem",
+                  color: "#18191d",
+                }}
               >
-                A taste of Stealthio disruption:
+                We want you to share in the success of the company. Register
+                with the Stealthio community now:
               </Typography>
 
-              <Grid container spacing={2}>
-                {techs.map((tech) => (
-                  <Grid item xs={12} md={6} lg={4}>
-                    <Box
-                      sx={{
-                        textAlign: "center",
-                        alignItems: "center",
-                        display: "flex",
-                        flexDirection: "column",
-                      }}
-                    >
-                      <Typography
-                        variant="h5"
-                        component="h4"
-                        sx={{ marginBottom: "2rem" }}
-                      >
-                        {tech.title}
-                      </Typography>
-
-                      <Typography
-                        component="p"
-                        sx={{ padding: "0 6rem", marginBottom: "1rem" }}
-                      >
-                        {tech.description}
-                      </Typography>
-                    </Box>
-                  </Grid>
-                ))}
-              </Grid>
+              <ul
+                style={{
+                  padding: "0 0 0 1rem",
+                  color: "#18191d",
+                }}
+              >
+                <li>
+                  get details of how to secure your free company share-options
+                </li>
+                <li>get early-bird access to the private token sale</li>
+                <li>
+                  get community access to the strictly limited NFT auction
+                </li>
+                <li>get the chance to win free annual licences</li>
+                <li>plus many other benefits and giveaways</li>
+              </ul>
+            </Box>
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={12} md={5}>
+            <Box
+              sx={{
+                textAlign: "center",
+                alignItems: "left",
+                display: "flex",
+                flexDirection: "column",
+                padding: { xs: "3rem", md: "0" },
+              }}
+            >
+              <TextField
+                id="company"
+                variant="outlined"
+                label="Company name"
+                type="text"
+                required
+                sx={{ marginBottom: "1rem" }}
+              />
+              <TextField
+                id="name"
+                variant="outlined"
+                label="Your name"
+                type="text"
+                required
+                sx={{ marginBottom: "1rem" }}
+              />
+              <TextField
+                id="email"
+                variant="outlined"
+                label="Email"
+                type="email"
+                required
+                sx={{ marginBottom: "1rem" }}
+              />
+              <Button variant="outlined">Keep me informed</Button>
             </Box>
           </Grid>
         </Grid>
       </Container>
-      <Container id="team" sx={{ padding: "5rem 0 5rem" }}>
+
+      <Container
+        id="team"
+        maxWidth={false}
+        sx={{ padding: "5rem 0 5rem", background: "#1e132c" }}
+      >
         <Grid container spacing={2}>
-          <Grid item xs={12}>
+          <Grid item xs={1} md={2}></Grid>
+          <Grid item xs={11} md={6}>
             <Box
               sx={{
-                textAlign: "center",
-                alignItems: "center",
+                textAlign: "left",
+                alignItems: "left",
                 display: "flex",
                 flexDirection: "column",
               }}
             >
               <Typography
                 variant="h5"
-                component="h4"
-                sx={{ marginBottom: "2rem" }}
+                component="p"
+                sx={{ marginBottom: "2rem", color: "#e8e8e8" }}
               >
                 MEET THE COLLECTIVE
               </Typography>
 
               <Typography
                 component="p"
-                sx={{ padding: "0 6rem", marginBottom: "1rem" }}
+                sx={{
+                  padding: { xs: "1rem", md: "0 0 0 5rem" },
+                  marginBottom: "1rem",
+                  color: "#e8e8e8",
+                }}
               >
                 Unified by a shared vision of the possibilities of disruptive
                 technology and how these technologies can be used to deliver a
                 true paradigm-shift in the way we do recruitment.
               </Typography>
 
-              <Typography component="p" sx={{ marginBottom: "1rem" }}>
+              <Typography
+                component="p"
+                sx={{
+                  marginBottom: "1rem",
+                  padding: { xs: "1rem", md: "0 0 0 5rem" },
+                  color: "#e8e8e8",
+                }}
+              >
                 Here's what were bring to the collective.
               </Typography>
 
-              <Typography component="p" sx={{ marginBottom: "3rem" }}>
+              <Typography
+                component="p"
+                sx={{
+                  marginBottom: "3rem",
+                  padding: { xs: "1rem", md: "0 0 0 5rem" },
+                  color: "#e8e8e8",
+                }}
+              >
                 Contact us if you believe that you could add value to the team.
               </Typography>
             </Box>
@@ -519,34 +610,116 @@ function App() {
             <Grid item xs={12} md={4} lg={3}>
               <Box
                 sx={{
-                  textAlign: "center",
-                  alignItems: "center",
+                  textAlign: "left",
+                  alignItems: { xs: "center", md: "left" },
                   display: "flex",
+                  paddingRight: { xs: "0rem", md: "3rem" },
                   flexDirection: "column",
+                  marginBottom: "1rem",
                 }}
               >
-                <Avatar
-                  sx={{ width: 200, height: 200, bgcolor: deepOrange[500] }}
-                  alt={person.name}
-                  src={person.image}
+                <a href={person.linkedin} target={"_blank"}>
+                  <img
+                    style={{
+                      width: 200,
+                      height: 200,
+                      borderRadius: "20px",
+                      border: "5px solid #e8e8e8",
+                    }}
+                    alt={person.name}
+                    src={person.image}
+                  />
+                </a>
+                <Typography
+                  variant="h6"
+                  component="p"
+                  sx={{ color: "#e8e8e8", textAlign: "left" }}
                 >
-                  {!person.linkedin ? `AMD` : ""}
-                </Avatar>
-                <Typography variant="h6" component="p">
                   {person.name}
                 </Typography>
-                <Typography component="h3">{person.role}</Typography>
+                <Typography component="h3" sx={{ color: "#e8e8e8" }}>
+                  {person.role}
+                </Typography>
               </Box>
             </Grid>
           ))}
         </Grid>
       </Container>
 
-      <img
-        style={{ marginTop: "5rem", width: "100%" }}
-        src="/Stelthio_pattern-dark.png"
-        alt=""
-      />
+      <Container
+        id="technology"
+        sx={{ padding: "5rem 0 5rem", background: "#18191d" }}
+        maxWidth={false}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                textAlign: "left",
+                alignItems: "left",
+                display: "flex",
+                flexDirection: "column",
+                padding: { xs: " 1rem", md: 0 },
+              }}
+            >
+              <Typography
+                variant="h5"
+                component="p"
+                sx={{ marginBottom: "1rem", color: "#e8e8e8" }}
+              >
+                TECHNOLOGY
+              </Typography>
+
+              <Typography
+                component="p"
+                sx={{
+                  padding: "0",
+                  marginBottom: "5rem",
+                  color: "#e8e8e8",
+                }}
+              >
+                A taste of Stealthio disruption:
+              </Typography>
+
+              <Grid container spacing={2}>
+                {techs.map((tech) => (
+                  <Grid item xs={12} md={6} lg={4}>
+                    <Box
+                      sx={{
+                        textAlign: "left",
+                        alignItems: "left",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Typography
+                        variant="h5"
+                        component="p"
+                        sx={{ marginBottom: "2rem", color: "#e8e8e8" }}
+                      >
+                        {tech.title}
+                      </Typography>
+
+                      <Typography
+                        component="p"
+                        sx={{
+                          padding: "0",
+                          marginBottom: "1rem",
+                          color: "#e8e8e8",
+                        }}
+                      >
+                        {tech.description}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <img style={{ width: "100%" }} src="/Stelthio_pattern-dark.png" alt="" />
     </>
   );
 }
